0e5d7716c98993597bbe3647a60ed0033f1ba2a5
pedramkarimii <pedram.9060@gmail.com>
2024-02-13 14:23:36 -0500

    Update Views add class CountAllView

--- music/views.py
+++ music/views.py
@@ -1,175 +1,83 @@
-# from django.http import Http404
-# from django.urls import reverse
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import TemplateView, ListView, DeleteView
+from django.views.generic import ListView
 from django.views.generic.edit import View
-# from django.views import View
 from .forms import MusicForm
-from .models import Music, Artist, Genre, Album
-# from .serializers import MusicSerializer
+from .models import Music
 from django.contrib import messages
 from django.urls import reverse_lazy
-
-
-# from django.http import Http404
-
-
-#
-# class MusicAddView(View):
-#     @staticmethod
-#     def get(request):
-#         return (form := MusicForm()) and render(request, 'Music/add.html', {'form': form})
-#
-#     @staticmethod
-#     def post(request):
-#         form = MusicForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             Music.objects.create(title=cd['title'], artist=cd['artist'], album=cd['album'], year=cd['year'],
-#                                  genre=cd['genre'], image=cd['image'], audio=cd['audio'])
-#             messages.success(request, 'Music added successfully', extra_tags='success')
-#             return redirect('info')
-#         return render(request, 'Music/add.html', {'form': form})
-# class MusicAddView(CreateView):
-#     model = Music
-#     form_class = MusicForm
-#     template_name = 'Music/add.html'
-#     success_url = reverse_lazy('info')
-#
-#     def form_valid(self, form):
-#         messages.success(self.request, 'Music added successfully', extra_tags='success')
-#         return super().form_valid(form), redirect('Main')
+from django.db.models import Count
 
 
 class MusicAddView(View):
     model = Music
-    form_class = MusicForm
+    form_class = MusicForm()
     template_name = 'Music/add.html'
     success_url = reverse_lazy('info')
 
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        print(form , "formmmmmmmmmmmmmmmmm")
+        form = MusicForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            print(cd , "cdddddddddddddddddddddddd")
-            if Music.objects.exists():
-                messages.error(request, 'Music already exists')
-            else:
-                # Create the Music object
-                Music.objects.create(
-                    title=cd.get('title'),
-                    artist=cd.get('artist'),
-                    album=cd.get('album'),
-                    genres=cd.get('genres'),
-                    category=cd.get('category'),
-                    release_date=cd.get('release_date')
-                )
+            self.model.objects.create(
+                title=cd['title'],
+                artist=cd['artist'],
+                album=cd['album'],
+                release_date=cd['release_date'],
+                # genres=cd['genres'],
+                category=cd['category']
+                # created_at=cd['created_at'],
+                # modified_at=cd['modified_at']
+            )
+            print(form, "moooooooooodeeeeeeeeeeeeellllllll")
 
-                messages.success(request, 'Music added successfully', extra_tags='success')
-                return redirect('info')
+            messages.success(request, 'Music added successfully', extra_tags='success')
+            return redirect('info')
         else:
-            # If the form is not valid, render it again with errors
             return render(request, self.template_name, {'form': form})
 
     def get(self, request, *args, **kwargs):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
 
+        return (form := MusicForm()) and render(request, self.template_name, {'form': form})
     # def dispatch(self, request, *args, **kwargs):
-    #     if request.method == 'POST' and Music.objects.exists():
-    #         raise Http404("Music object already exists")
+    #     if Music.objects.count() > 0:
+    #         messages.warning(request, 'Music already exists', extra_tags='warning')
+    #         return redirect('info')
     #     return super().dispatch(request, *args, **kwargs)
 
-    # def get(self, request, *args, **kwargs):
-    #     form = self.form_class()
-    #     return render(request, self.template_name, {'form': form})
 
-
-# class MusicInfoView(View):
-#     @staticmethod
-#     def get(request):
-#         music_data = Music.objects.all()
-#         serializer = MusicSerializer(music_data, many=True)
-#         return render(request, 'Music/info.html', {'music_data': serializer.data})
-class CountAllView(ListView):
+class MusicDeleteView(View):
     model = Music
-    template_name = 'Music/info.html'
-    context_object_name = 'music_data'
-
-
-# class MusicDeleteView(View):
-#     @staticmethod
-#     def get(request, music_id):
-#         (music := get_object_or_404(Music, pk=music_id)) and music.delete()
-#         messages.success(request, 'Music deleted successfully', extra_tags='success')
-#         return redirect('info')
-# class MusicDeleteView(DeleteView):
-#     model = Music
-#     success_url = reverse_lazy('info')
-#
-#     def delete(self, request, *args, **kwargs):
-#         self.object = self.get_object()
-#         self.object.delete()
-#         messages.success(request, 'Music deleted successfully', extra_tags='success')
-#         return super().delete(request, *args, **kwargs), redirect('Main')
-
-class MusicDeleteView(DeleteView):
-    model = Music
-    template_name = 'Music/delete.html'
     success_url = reverse_lazy('info')
 
-    # def get_object(self):
-    #     # Get object by id or title
-    #     queryset = self.get_queryset()
-    #     filter_kwargs = {'id': self.kwargs.get('pk')}
-    #     if queryset.filter(**filter_kwargs).exists():
-    #         return queryset.get(**filter_kwargs)
-    #     filter_kwargs = {'title': self.kwargs.get('pk')}
-    #     if queryset.filter(**filter_kwargs).exists():
-    #         return queryset.get(**filter_kwargs)
-    #     return None
-    def __init__(self, **kwargs):
-        super().__init__(kwargs)
-        self.object = None
-
-    def delete(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        self.object.delete()
+    def get(self, request, pk):
+        if isinstance(pk, int):
+            (music := get_object_or_404(self.model, pk=pk)) and music.delete()
+        else:
+            (music := get_object_or_404(self.model, title=pk)) and music.delete()
         messages.success(request, 'Music deleted successfully', extra_tags='success')
-        return super().delete(request, *args, **kwargs)
-
-    def get_object(self, queryset=None):
-        """
-        Get the object to be deleted. Raises a 404 error if the object does not exist.
-        """
-        queryset = self.get_queryset()
-        obj = get_object_or_404(queryset, pk=self.kwargs.get('pk'))
-        return obj
+        return redirect('info')
 
 
-# def count_all(request):
-#     artist_count = Artist.objects.count()
-#     album_count = Album.objects.count()
-#     genre_count = Genre.objects.count()
-#     music_count = Music.objects.count()
-#
-#     context = {
-#         'artist_count': artist_count,
-#         'album_count': album_count,
-#         'genre_count': genre_count,
-#         'music_count': music_count,
-#     }
-#     return render(request, 'Music/count.html', context)
+class MusicInfoView(View):
+    template_name = 'Music/info.html'
+
+    def get(self, request):
+        return (form := Music.objects.all()) and render(request, self.template_name, {'form': form})
 
 
-class MusicInfoView(TemplateView):
+class CountAllView(ListView):
+    model = Music
     template_name = 'Music/count.html'
+    context_object_name = 'music_data'
+    queryset = Music.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['artist_count'] = Artist.objects.count()
-        context['album_count'] = Album.objects.count()
-        context['genre_count'] = Genre.objects.count()
-        context['music_count'] = Music.objects.count()
+
+        # Calculate counts for each field
+        context['title_counts'] = Music.objects.values('title').annotate(count=Count('title'))
+        context['artist_counts'] = Music.objects.values('artist').annotate(count=Count('artist'))
+        context['album_counts'] = Music.objects.values('album').annotate(count=Count('album'))
+        context['category_counts'] = Music.objects.values('category').annotate(count=Count('category'))
+
         return context
