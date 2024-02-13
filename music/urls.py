7b77bbfbcbcad324043b9249224fa6298fb77809
pedramkarimii <pedram.9060@gmail.com>
2024-02-13 09:43:22 -0500

    Update urls

--- music/urls.py
+++ music/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('Main/', views.MusicInfoView.as_view(), name='Main'),
     path('add/', views.MusicAddView.as_view(), name='add'),
-    path('info/', views.CountAllView.as_view(), name='info'),  # Add this line
+    path('info/', views.CountAllView.as_view(), name='info'),
     path('delete/<int:pk>/', views.MusicDeleteView.as_view(), name='delete'),
+    path('delete/<str:pk>/', views.MusicDeleteView.as_view(), name='delete'),
 ]
